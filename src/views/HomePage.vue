44f5a43953d4191550cea141d2e5fdc6a5c1dc53
Kris Marshall <rflctnofu@hotmail.com>
2023-04-05 18:59:24 -0400

    bug?

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,12 @@
 import qs from "../data/questions";
 import { ref } from "vue";
 
+const index = ref(0);
 const questions = ref(qs);
 </script>
-<template></template>
+<template>
+  <div class="container">
+    <h2>{{ questions[index].title }}</h2>
+  </div>
+</template>
 <style scoped></style>
