c20586d14254bc0a35fc0dfcac7baaf0af092426
Kris Marshall <rflctnofu@hotmail.com>
2023-04-05 18:55:01 -0400

    basic files setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header id="header">
+    <div id="highscores"></div>
+    <p class="welcome">Code Quiz!!</p>
+  </header>
+  <RouterView />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#header {
+  font-size: x-large;
+  color: antiquewhite;
+  max-height: 200px;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,7 +1,8 @@
 <script setup>
+import qs from "../data/questions";
 import { ref } from "vue";
+
+const questions = ref(qs);
 </script>
-
 <template></template>
-
 <style scoped></style>
